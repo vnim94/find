ca85b3afa6c32e8ab80c6c571aa8ce60794a57ba
vnim94 <victor.nim94@outlook.com>
2022-03-26 17:17:34 +1100

    added base styling

---
+++ client/src/header/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <div className="header">
+            <div className="domain flex flex-jc-c">
+                <a href="/">Jobs</a>
+                <a href="/">Courses</a>
+                <a href="/">Businesses for sale</a>
+                <a href="/">Volunteering</a>
+            </div>
+            <div className="country flex flex-jc-c">
+                <a href="/">AU</a>
+                <a href="/">NZ</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
