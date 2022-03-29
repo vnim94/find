22f82ed2674af13861fae1b4381e7538334abb76
vnim94 <victor.nim94@outlook.com>
2022-03-29 19:15:52 +1100

    added app model and types

---
+++ server/src/app/app.types.js
@@ -0,0 +1,25 @@
+const AppTypes = `
+    type App {
+        id: ID!
+        applicant: User!
+        job: Job!
+        resume: String!
+        letter: String!
+        status: String!
+    }
+
+    union AppResult = App | NotFound
+
+    type Query {
+        app(id: ID!): AppResult
+        apps(user: ID): [App]
+    }
+
+    type Mutation {
+        createApp(applicant: ID!, job: ID!, resume: String, letter: String, status: String!): App
+        updateApp(id: ID!, resume: String, letter: String, status: String): AppResult
+        deleteApp(id: ID!): AppResult
+    }
+`
+
+module.exports = AppTypes;
