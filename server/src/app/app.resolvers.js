9e8e6cab24e61d401f7dabd1ebd39197008c24dd
vnim94 <victor.nim94@outlook.com>
2022-03-29 19:15:52 +1100

    added app model and types

---
+++ server/src/app/app.resolvers.js
@@ -0,0 +1,26 @@
+const AppResolver = {
+    AppResult: {
+        __resolveType: (_) => {
+
+        }
+    },
+    Query: {
+        app: async (_, { id }) => {
+            
+        },
+        apps: async(_, args) => {
+
+        }
+    },
+    Mutation: {
+        createApp: async (_, args, context) => {
+
+        },
+        updateApp: async (_, args, context) => {
+
+        },
+        deleteApp: async (_, { id }, context) => {
+
+        }
+    }
+}
