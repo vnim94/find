d38f5a346b35c83ad60b795fffd072f4adea5898
vnim94 <victor.nim94@outlook.com>
2022-04-03 18:14:32 +1000

    added and styled content cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from './header/Header';
 import Search from './search/Search';
 import Recent from './recent/Recent';
 import Dashboard from './dashboard/Dashboard';
+import Content from './content/Content';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
         <Search />
         <Recent />
         <Dashboard />
+        <Content /> 
         </>
     )
 }
