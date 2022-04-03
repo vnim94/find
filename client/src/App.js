d2fc64ab1942ee192612cb76ee711b71b60a69a6
vnim94 <victor.nim94@outlook.com>
2022-04-03 10:43:28 +1000

    added and styled recent section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Header from './header/Header';
 import Search from './search/Search';
+import Recent from './recent/Recent';
 
 function App() {
     return (
         <>
         <Header />
         <Search />
+        <Recent />
         </>
     )
 }
--- client/src/search/Options.js
+++ client/src/search/Options.js
@@ -2,13 +2,17 @@ import './Options.css';
 import { useState } from 'react';
 
 function Options() {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
+        <>
         <div className="options flex flex-jc-c">
             <div className="page flex flex-row">
                 <span className="light-black">|</span>
                 <Option value="All work types"/>
                 <span className="light-black">|</span>
-                <Option text="paying" value="$0" />
+                <Option text="paying" value="$0"/>
                 <span className="light-black">|</span>
                 <Option text="to" value="$200k+" />
                 <span className="light-black">|</span>
@@ -16,22 +20,51 @@ function Options() {
                 <span className="light-black">|</span>
             </div>
         </div>
+        {expanded && <ExpandedOption />}
+        </>
     )
 }
 
 function Option(props) {
 
-    const [expanded, setExpanded] = useState(false);
+    const { expand, expanded, text, value } = props;
 
     return (
-        <div className="option flex flex-row flex-jc-c" onClick={() => setExpanded(!expanded)}>
+        <div className="option flex flex-row flex-jc-c" onClick={() => expand(!expanded)}>
             <div className="flex">
-                {props.text && <span className="grey">{props.text}</span>}
-                <span className="white">{props.value}</span>
+                {text && <span className="grey">{text}</span>}
+                <span className="white">{value}</span>
                 <span className={`${expanded && 'flip'} white expand material-icons-outlined`}>expand_more</span>
             </div>
         </div>
     )
 }
+
+function ExpandedOption() {
+    return (
+        <div className="expanded-option flex flex-jc-c">
+            <nav className="page">
+                <ul className="expanded-sub-options flex flex-row flex-jc-sb">
+                    <SubOption text="Full time" />
+                    <SubOption text="Part time" />
+                    <SubOption text="Contract/Temp" />
+                    <SubOption text="Casual/Vacation" />
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+function SubOption(props) {
+
+    const [selected, setSelected] = useState(false);
+
+    return (
+        <li className={selected && 'selected-sub-option'} onClick={() => setSelected(!selected)}>
+            <span>{props.text}</span>
+        </li>
+    )
+}
+
 
 export default Options;
