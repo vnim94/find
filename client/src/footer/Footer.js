f1ffd34f8b0287b515b3743258c0cc2ae0f56a30
vnim94 <victor.nim94@outlook.com>
2022-04-03 19:58:05 +1000

    updated styling in footer

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -7,50 +7,65 @@ function Footer() {
                 <div className="flex flex-row">
                     <div className="column">
                         <span><b>Tools</b></span>
-                        <a href="/">Job search</a>
-                        <a href="/">Profile</a>
-                        <a href="/">Recommended jobs</a>
-                        <a href="/">Saved searches</a>
-                        <a href="/">Saved jobs</a>
-                        <a href="/">Applied jobs</a>
-                        <a href="/">Career Advice</a>
-                        <a href="/">Explore Careers</a> 
-                        <a href="/">Company reviews</a>
-                        <a href="/">Download apps</a> 
-                        <a href="/">Find sites</a>
+                        <a className="footer-link" href="/">Job search</a>
+                        <a className="footer-link" href="/">Profile</a>
+                        <a className="footer-link" href="/">Recommended jobs</a>
+                        <a className="footer-link" href="/">Saved searches</a>
+                        <a className="footer-link" href="/">Saved jobs</a>
+                        <a className="footer-link" href="/">Applied jobs</a>
+                        <a className="footer-link" href="/">Career Advice</a>
+                        <a className="footer-link" href="/">Explore Careers</a> 
+                        <a className="footer-link" href="/">Company reviews</a>
+                        <div className="expandable-link flex flex-ai-c">
+                            <a className="footer-link" href="/">Download apps</a> 
+                            <span className="material-icons-outlined">expand_more</span>
+                        </div>
+                        <div className="expandable-link flex flex-ai-c">
+                            <a className="footer-link" href="/">Find sites</a>
+                            <span className="material-icons-outlined">expand_more</span>
+                        </div>
                     </div>
                     <div className="column">
                         <span><b>Company</b></span>
-                        <a href="/">About Find</a>
-                        <a href="/">Newsroom</a>
-                        <a href="/">Investors</a>
-                        <a href="/">International partners</a>
-                        <a href="/">Partner services</a>
+                        <a className="footer-link" href="/">About Find</a>
+                        <a className="footer-link" href="/">Newsroom</a>
+                        <a className="footer-link" href="/">Investors</a>
+                        <div className="expandable-link flex flex-ai-c">
+                            <a className="footer-link" href="/">International partners</a>
+                            <span className="material-icons-outlined">expand_more</span>
+                        </div>
+                        <div className="expandable-link flex flex-ai-c">
+                            <a className="footer-link" href="/">Partner services</a>
+                            <span className="material-icons-outlined">expand_more</span>
+                        </div>
                     </div>
                     <div className="column">
                         <span><b>Connect</b></span>
-                        <a href="/">Help centre</a>
-                        <a href="/">Contact us</a>
-                        <a href="/">Work for Find</a>
-                        <a href="/">Product &amp; Tech Blog</a>
-                        <a href="/">Find videos</a>
-                        <a href="/">Social</a>
+                        <a className="footer-link" href="/">Help centre</a>
+                        <a className="footer-link" href="/">Contact us</a>
+                        <a className="footer-link" href="/">Work for Find</a>
+                        <a className="footer-link" href="/">Product &amp; Tech Blog</a>
+                        <a className="footer-link" href="/">Find videos</a>
+                        <div className="expandable-link flex flex-ai-c">
+                            <a className="footer-link" href="/">Social</a>
+                            <span className="material-icons-outlined">expand_more</span>
+                        </div>
                     </div>
                     <div className="column">
                         <span><b>Employers</b></span>
-                        <a href="/">Register for free</a>
-                        <a href="/">Post a job as</a>
-                        <a href="/">Products &amp; prices</a>
-                        <a href="/">Customer service</a>
-                        <a href="/">Hiring Advice</a>
-                        <a href="/">Market Insights</a>
-                        <a href="/">Recruitment software partners</a>
+                        <a className="footer-link" href="/">Register for free</a>
+                        <a className="footer-link" href="/">Post a job as</a>
+                        <a className="footer-link" href="/">Products &amp; prices</a>
+                        <a className="footer-link" href="/">Customer service</a>
+                        <a className="footer-link" href="/">Hiring Advice</a>
+                        <a className="footer-link" href="/">Market Insights</a>
+                        <a className="footer-link" href="/">Recruitment software partners</a>
                     </div>
                 </div>
                 <div className="terms">
-                    <a href="/">Privacy</a>
-                    <a href="/">Terms &amp; Conditions</a>
-                    <a href="/">Protect yourself online</a>
+                    <a className="footer-link" href="/">Privacy</a>
+                    <a className="footer-link" href="/">Terms &amp; Conditions</a>
+                    <a className="footer-link" href="/">Protect yourself online</a>
                     <span>© Find. All rights reserved</span>
                 </div>
             </div>
