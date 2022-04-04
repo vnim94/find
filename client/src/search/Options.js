2b57fae6712b4011f5e613ca7b6adc0b63908fc5
vnim94 <victor.nim94@outlook.com>
2022-04-04 19:46:26 +1000

    fixed work type option bug

--- client/src/search/Options.js
+++ client/src/search/Options.js
@@ -57,31 +57,31 @@ function Options() {
         <div className="options flex flex-jc-c">
             <div className="page flex flex-row">
                 <span className="light-black">|</span>
-                <Option selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text={workType.length === 0 || workType.length === workTypes.length ? 'All work types' : (workType.length > 1 ? `${workType.length} work types` : workType[0])} />
+                <Option id="workType" selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text={workType.length === 0 || workType.length === workTypes.length ? 'All work types' : (workType.length > 1 ? `${workType.length} work types` : workType[0])} />
                 <span className="light-black">|</span>
-                <Option selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="paying" value={payBase === '$0' ? '$0' : `$${payBase}`}/>
+                <Option id="payBase" selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="paying" value={payBase === '$0' ? '$0' : `$${payBase}`}/>
                 <span className="light-black">|</span>
-                <Option selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="to" value={`$${payCeiling}`} />
+                <Option id="payCeiling" selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="to" value={`$${payCeiling}`} />
                 <span className="light-black">|</span>
-                <Option selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="listed" value={time.toLowerCase()} />
+                <Option id="time" selected={selected} setSelected={setSelected} expanded={expanded} setExpanded={setExpanded} text="listed" value={time.toLowerCase()} />
                 <span className="light-black">|</span>
             </div>
         </div>
-        {selected !== undefined && !['paying','to','listed'].includes(selected) ? <ExpandedOption type='multi' values={workTypes} display={workType} setDisplay={setWorkType} /> : undefined}
-        {selected === 'paying' ? <ExpandedOption values={payBases} setDisplay={setPayBase}/> : undefined}
-        {selected === 'to' ? <ExpandedOption values={payCeilings} setDisplay={setPayCeiling}/> : undefined}
-        {selected === 'listed' ? <ExpandedOption values={times} setDisplay={setTime}/> : undefined}
+        {selected === "workType" ? <ExpandedOption type='multi' values={workTypes} display={workType} setDisplay={setWorkType} /> : undefined}
+        {selected === "payBase" ? <ExpandedOption values={payBases} selectedSubOption={payBase} setDisplay={setPayBase}/> : undefined}
+        {selected === "payCeiling" ? <ExpandedOption values={payCeilings} selectedSubOption={payCeiling} setDisplay={setPayCeiling}/> : undefined}
+        {selected === "time" ? <ExpandedOption values={times} selectedSubOption={time} setDisplay={setTime}/> : undefined}
         </>
     )
 }
 
 function Option(props) {
 
-    const { expanded, setExpanded, selected, setSelected, text, value } = props;
+    const { id, expanded, setExpanded, selected, setSelected, text, value } = props;
 
     const handleClick = () => {
-        expanded === text ? setExpanded(undefined) : setExpanded(text);
-        selected === text ? setSelected(undefined) : setSelected(text);
+        expanded === id ? setExpanded(undefined) : setExpanded(id);
+        selected === id ? setSelected(undefined) : setSelected(id);
     }
 
     return (
@@ -89,14 +89,14 @@ function Option(props) {
             <div className="flex">
                 {!value ? 
                 <>
-                    <span className={selected !== undefined && selected !== 'All work types' ? 'black' : 'white'}>{text}</span>
-                    <span className={`${expanded !== undefined ? 'flip' : undefined} ${selected !== undefined && selected !== 'All work types' ? 'black' : 'white'} material-icons-outlined`}>expand_more</span>
+                    <span className={selected !== undefined && selected !== id ? 'black' : 'white'}>{text}</span>
+                    <span className={`${expanded !== undefined ? 'flip' : undefined} ${selected !== undefined && selected !== id ? 'black' : 'white'} material-icons-outlined`}>expand_more</span>
                 </>
                 :
                 <>
-                    <span className={selected !== undefined && selected !== text ? 'black' : 'grey'}>{text}</span>
-                    <span className={selected !== undefined && selected !== text ? 'black' : 'white'}>{value}</span>
-                    <span className={`${expanded !== undefined && expanded === text ? 'flip' : undefined} ${selected !== undefined && selected !== text ? 'black' : 'white'} material-icons-outlined`}>expand_more</span>
+                    <span className={selected !== undefined && selected !== id ? 'black' : 'grey'}>{text}</span>
+                    <span className={selected !== undefined && selected !== id ? 'black' : 'white'}>{value}</span>
+                    <span className={`${expanded !== undefined && expanded === id ? 'flip' : undefined} ${selected !== undefined && selected !== id ? 'black' : 'white'} material-icons-outlined`}>expand_more</span>
                 </>
                 }
             </div>
@@ -106,17 +106,16 @@ function Option(props) {
 
 function ExpandedOption(props) {
 
-    const { type, values, display, setDisplay } = props;
-    const [selectedSubOption, setSubOption] = useState();
+    const { display, setDisplay, selectedSubOption, type, values,  } = props;
 
     return (
         <div className="expanded-option flex flex-jc-c">
             <nav className="page">
                 <ul className="expanded-sub-options flex flex-row flex-jc-sb">
                     {type ? 
-                        values.map((value,index) => { return <MultiSubOption key={index} display={display} setDisplay={setDisplay} text={value} /> })
+                        values.map((value,index) => { return <MultiSubOption key={index} display={display} selectedSubOption={selectedSubOption} setDisplay={setDisplay} text={value} /> })
                     : 
-                        values.map((value,index) => { return <SingleSubOption key={index} setDisplay={setDisplay} selectedSubOption={selectedSubOption} setSelectedSubOption={setSubOption} text={value} /> })
+                        values.map((value,index) => { return <SingleSubOption key={index} setDisplay={setDisplay} selectedSubOption={selectedSubOption} text={value} /> })
                     }
                 </ul>
             </nav>
@@ -127,15 +126,13 @@ function ExpandedOption(props) {
 function MultiSubOption(props) {
 
     const { text, display, setDisplay } = props;
-    const [selected, setSelected] = useState(false);
 
     const handleClick = () => {
-        setSelected(!selected);
         display.indexOf(text) > -1 ? setDisplay(display.filter(type => type !== text)) : setDisplay([ ...display, text]);
     }
 
     return (
-        <li className={selected ? 'selected-sub-option' :  undefined} onClick={handleClick}>
+        <li className={display.includes(text) ? 'selected-sub-option' :  undefined} onClick={handleClick}>
             <span>{text}</span>
         </li>
     )
@@ -143,10 +140,9 @@ function MultiSubOption(props) {
 
 function SingleSubOption(props) {
 
-    const { setDisplay, selectedSubOption, setSelectedSubOption, text} = props;
+    const { setDisplay, selectedSubOption, text} = props;
 
     const handleClick = () => {
-        setSelectedSubOption(text);
         setDisplay(text);
     }
 
