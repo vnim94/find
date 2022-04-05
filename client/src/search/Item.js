f0a34f1a71739912d9174b520122f9ac9d13803c
vnim94 <victor.nim94@outlook.com>
2022-04-05 12:00:08 +1000

    implemented clear button toggle

--- client/src/search/Item.js
+++ client/src/search/Item.js
@@ -2,11 +2,16 @@ import { useState } from 'react';
 
 function Item(props) {
 
-    const { classification, jobCount } = props;
-    const [checked, setChecked] = useState(false);
+    const { classification, classifications, setClassifications, jobCount } = props;
+    const [checked, setChecked] = useState(classifications.indexOf(classification) > -1);
+
+    const handleClick = () => {
+        setChecked(!checked);
+        classifications.indexOf(classification) > -1 ? setClassifications(classifications.filter(c => c !== classification)) : setClassifications([ ...classifications, classification]);
+    }
 
     return (
-        <li key={classification.toLowerCase()} className="flex flex-row flex-jc-sb flex-ai-c" onClick={() => setChecked(!checked)}>
+        <li key={classification.toLowerCase()} className="flex flex-row flex-jc-sb flex-ai-c" onClick={handleClick}>
             <div className="flex flex-ai-c">
                 <span className={`${checked && 'checked'} checkbox`}></span>
                 <span>{classification}</span>
--- client/src/search/Search.js
+++ client/src/search/Search.js
@@ -7,6 +7,40 @@ function Search() {
 
     const [visible, setVisible] = useState(false);
     const [expanded, setExpanded] = useState(false);
+    const [classifications, setClassifications] = useState([]);
+
+    const data = [
+        { classification: 'Accounting', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Administration & Office Support', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Advertising, Arts & Media', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Banking & Financial Services', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Call Centre & Customer Service', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'CEO & General Management', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Community Services & Development', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Construction', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Consulting & Strategy', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Design & Architecture', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Education & Training', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Engineering', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Farming, Animals & Conservation', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Government & Defence', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Healthcare & Medical', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Hospitality & Tourism', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Human Resources & Recruitment', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Information & Communication Technology', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Insurance & Superannuation', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Legal', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Manufacturing, Transport & Logistics', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Marketing & Communications', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Mining, Resources & Energy', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Real Estate & Property', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Retail & Consumer Products', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Sales', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Science & Technology', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Self Employment', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Sport & Recreation', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) },
+        { classification: 'Trades & Services', jobCount: (Math.random() * 10000).toLocaleString(undefined, { maximumFractionDigits: 0 }) }
+    ];
 
     return (
         <>
@@ -19,10 +53,17 @@ function Search() {
                             <div className="flex flex-row">
                                 <input className="form-control" type="text" placeholder="Enter Keywords"></input>
                                 <div className={`classification form-control flex flex-ai-c flex-jc-sb ${visible && 'outlined'}`} onClick={() => setVisible(!visible)}>
-                                    <span className="dark-grey">Any Classification</span>
-                                    <span className={`${visible && 'flip'} material-icons-outlined`}>expand_more</span>
+                                    <span className="dark-grey">
+                                        {classifications.length === 0 ? 'Any classification' : classifications.length > 1 ? `${classifications.length} classifications` : classifications[0]}
+                                    </span>
+                                    <div className={`${classifications.length > 0 && 'shrink'} list-action flex flex-ai-c`}>
+                                        <span className={`${visible && 'flip'} material-icons-outlined`}>expand_more</span>
+                                        <div className={`${classifications.length === 0 && 'hidden'} clear flex flex-ai-c`} onClick={() => setClassifications([])}>
+                                            <span className="medium material-icons-outlined">clear</span>
+                                        </div>
+                                    </div>
                                 </div>
-                                {visible && <Dropdown />}
+                                {visible && <Dropdown classifications={classifications} setClassifications={setClassifications} data={data}/>}
                             </div>
                         </div>
                         <div className="where flex flex-col">
