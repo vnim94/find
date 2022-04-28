562ca3fd4f31b947286c16490d09139069ac3d93
vnim94 <victor.nim94@outlook.com>
2022-04-28 18:38:38 +1000

    revert cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.options('*',cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));	
