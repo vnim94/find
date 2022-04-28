b34d0de688de3375836f314aac203c58070d21be
vnim94 <victor.nim94@outlook.com>
2022-04-28 18:36:58 +1000

    cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors());
+app.options('*',cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));	
