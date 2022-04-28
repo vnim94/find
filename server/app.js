b1a8b28ecb47421561de39ee4db24093f1da825f
vnim94 <victor.nim94@outlook.com>
2022-04-28 18:21:00 +1000

    revert cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors({ origin: "https://find-server-v1.herokuapp.com/" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));	
